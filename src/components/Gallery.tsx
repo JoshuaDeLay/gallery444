adaf993e239ccb72e6db7d68ec190f4a10af01c5
JoshuaDeLay <jdelay@usc.edu>
2025-02-14 15:54:54 -0800

    Create Gallery.tsx

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Gallery = () => {
+    return (
+        <div>
+            <h1>Gallery Component</h1>
+            {/* Gallery content */}
+        </div>
+    );
+};
